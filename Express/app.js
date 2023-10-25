66f8630235d5fe81d94554405d8a4863203563cd
Rashmikanta Parida <rashmikantaparida@Rashmikantas-MacBook-Air.local>
2023-10-25 15:43:55 +0530

    updated

--- Express/app.js
+++ Express/app.js
@@ -1,11 +1,21 @@
 const express = require('express');
-
+const fs = require('fs');
 const app = express()
 
 app.get('/', (req,res)=>{
     res.send('Hello');
 })
 
+
+app.get('/getDetailss', (req, res)=> {
+    fs.readFile('./../Files/temp.json', (error, result)=> {
+        if(error){
+            console.log(error);
+        } else {
+            res.send(JSON.parse(result));
+        }
+    })
+})
 app.listen(3000, ()=>{
     console.log('App started running ')
 });
